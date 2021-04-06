f6528703b00a4e54475beca55631c5a08c702464
darkace <dan270799@gmail.com>
2021-04-06 17:46:44 +0300

    resources fix

--- src/main/java/ru/admiralnsk/admiralbd/AdmiralbdApplication.java
+++ src/main/java/ru/admiralnsk/admiralbd/AdmiralbdApplication.java
@@ -1,16 +1,13 @@
 package ru.admiralnsk.admiralbd;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 
 @SpringBootApplication
 @EnableWebMvc
-public class AdmiralbdApplication  {
+public class AdmiralbdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdmiralbdApplication.class, args);
