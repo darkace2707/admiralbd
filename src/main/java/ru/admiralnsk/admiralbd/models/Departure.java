e3f55833f22aec4a843ab0ad3b0e5f78d61e8c28
Daniil <dan270799@gmail.com>
2021-04-06 18:05:56 +0300

    add departure model

---
+++ src/main/java/ru/admiralnsk/admiralbd/models/Departure.java
@@ -0,0 +1,51 @@
+package ru.admiralnsk.admiralbd.models;
+
+import lombok.*;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Departure {
+
+    private Integer id;
+    private Date departureDate;
+    private Integer carriageNumber;
+    private String documentNumber;
+    private Date arrivalDate;
+    private Date lendingDate;
+    private String transportationType;
+    private Integer cargo;
+    private String cargoType;
+    private String departureCountry;
+    private String departureStationCIS;
+    private Integer departureStationCISCode;
+    private String departureRegion;
+    private String departureWay;
+    private String departureStationRF;
+    private Integer departureStationRFCode;
+    private String consignor;
+    private Integer consignorACEO;
+    private String destinationCountry;
+    private String destinationRegion;
+    private String destinationWay;
+    private String destinationStationRF;
+    private Integer destinationStationRFCode;
+    private String destinationStationCIS;
+    private Integer destinationStationCISCode;
+    private String consignee;
+    private Integer consigneeACEO;
+    private String carriageKind;
+    private String carriageType;
+    private String payer;
+    private String owner;
+    private String renter;
+    private String operator;
+    private Integer carriageKm;
+    private Integer volume;
+    private Integer rate;
+
+}
