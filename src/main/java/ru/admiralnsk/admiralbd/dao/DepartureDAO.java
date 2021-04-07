c0c7b661485b596da35b1edc2fe4d4f475a9da00
poipoilion <poipoilion@users.noreply.github.com>
2021-04-07 16:43:41 +0600

    Created 4 tables wihtou values

--- src/main/java/ru/admiralnsk/admiralbd/dao/DepartureDAO.java
+++ src/main/java/ru/admiralnsk/admiralbd/dao/DepartureDAO.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -20,7 +21,13 @@ public class DepartureDAO {
         return jdbcTemplate.queryForObject("SELECT count(*) FROM departures WHERE DepartureWay = ?",
                 new Object[]{departureWay}, Integer.class);
     }
-
+    public List<String> getMonthes() {
+        List<String> monthes = new ArrayList();
+        for (int i = 1; i < 13; i++) {
+            monthes.add("month"+i);
+        }
+        return monthes;
+    }
     public List<String> getDistinctDepartureWays() {
         return jdbcTemplate.queryForList("SELECT DISTINCT departureWay FROM departures", String.class);
     }
