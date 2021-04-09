60f8ba41d3c8b27e5037b09f03bdeb323028c5ce
Daniil <dan270799@gmail.com>
2021-04-09 16:42:06 +0300

    added stream reading

--- src/main/java/ru/admiralnsk/admiralbd/parser/ExcelParser.java
+++ src/main/java/ru/admiralnsk/admiralbd/parser/ExcelParser.java
@@ -1,12 +1,17 @@
 package ru.admiralnsk.admiralbd.parser;
 
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellType;
 
+
+import com.monitorjbl.xlsx.StreamingReader;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 public class ExcelParser {
@@ -55,22 +60,37 @@ public class ExcelParser {
         return result;
     }*/
     public static boolean readFromExcel(String file) throws IOException{
-        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
-        XSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
-        XSSFRow row = myExcelSheet.getRow(0);
+//        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
+//        XSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
+//        XSSFRow row = myExcelSheet.getRow(0);
+//
+//        if(row.getCell(0).getCellType() == CellType.STRING){
+//            String name = row.getCell(0).getStringCellValue();
+//            System.out.println("name : " + name);
+//        }
+//
+//        if(row.getCell(0).getCellType() == CellType.NUMERIC){
+//            Date birthdate = row.getCell(0).getDateCellValue();
+//            System.out.println("birthdate :" + birthdate);
+//        }
+//
+//        myExcelBook.close();
 
-        if(row.getCell(0).getCellType() == CellType.STRING){
-            String name = row.getCell(0).getStringCellValue();
-            System.out.println("name : " + name);
+        InputStream is = new FileInputStream(new File(file));
+        Workbook workbook = StreamingReader.builder()
+                .rowCacheSize(5)    // number of rows to keep in memory (defaults to 10) Думаю около 30-50 можно грузить
+                .bufferSize(1024)     // buffer size to use when reading InputStream to file (defaults to 1024)
+                .open(is);
+
+        for (Sheet sheet : workbook){
+            System.out.println(sheet.getSheetName());
+            for (Row r : sheet) {
+                for (Cell c : r) {
+                    System.out.print(c.getStringCellValue() + " ");
+                }
+                System.out.println();
+            }
         }
-
-        if(row.getCell(0).getCellType() == CellType.NUMERIC){
-            Date birthdate = row.getCell(1).getDateCellValue();
-            System.out.println("birthdate :" + birthdate);
-        }
-
-        myExcelBook.close();
-
         return false;
     }
 
