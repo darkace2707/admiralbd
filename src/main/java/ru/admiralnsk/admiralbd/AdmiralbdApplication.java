7613d75b388ed4ed010b800dfbdeaca89f25776f
tus360 <43173691+tus360@users.noreply.github.com>
2021-04-09 19:49:49 +0700

    ExcelParser

--- src/main/java/ru/admiralnsk/admiralbd/AdmiralbdApplication.java
+++ src/main/java/ru/admiralnsk/admiralbd/AdmiralbdApplication.java
@@ -3,13 +3,18 @@ package ru.admiralnsk.admiralbd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ru.admiralnsk.admiralbd.parser.ExcelParser;
+
+import java.io.IOException;
 
 
 @SpringBootApplication
 @EnableWebMvc
 public class AdmiralbdApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(ExcelParser.readFromExcel("excel1.xlsx"));
         SpringApplication.run(AdmiralbdApplication.class, args);
     }
 
---
+++ src/main/java/ru/admiralnsk/admiralbd/parser/ExcelParser.java
@@ -0,0 +1,77 @@
+package ru.admiralnsk.admiralbd.parser;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
+public class ExcelParser {
+
+    /*public static String parse(String fileName) {
+        //инициализируем потоки
+        String result = "";
+        InputStream inputStream = null;
+        XSSFWorkbook workBook = null;
+        try {
+            inputStream = new FileInputStream(fileName);
+            workBook = new XSSFWorkbook(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //разбираем первый лист входного файла на объектную модель
+        Sheet sheet = workBook.getSheetAt(0);
+        Iterator<Row> it = sheet.iterator();
+        //проходим по всему листу
+        while (it.hasNext()) {
+            Row row = it.next();
+            Iterator<Cell> cells = row.iterator();
+            while (cells.hasNext()) {
+                Cell cell = cells.next();
+                CellType cellType = cell.getCellType();
+                //перебираем возможные типы ячеек
+                switch (cellType) {
+                    case STRING:
+                        result += cell.getStringCellValue() + "=";
+                        break;
+                    case NUMERIC:
+                        result += "[" + cell.getNumericCellValue() + "]";
+                        break;
+
+                    case FORMULA:
+                        result += "[" + cell.getNumericCellValue() + "]";
+                        break;
+                    default:
+                        result += "|";
+                        break;
+                }
+            }
+            result += "\n";
+        }
+
+        return result;
+    }*/
+    public static boolean readFromExcel(String file) throws IOException{
+        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
+        XSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
+        XSSFRow row = myExcelSheet.getRow(0);
+
+        if(row.getCell(0).getCellType() == CellType.STRING){
+            String name = row.getCell(0).getStringCellValue();
+            System.out.println("name : " + name);
+        }
+
+        if(row.getCell(0).getCellType() == CellType.NUMERIC){
+            Date birthdate = row.getCell(1).getDateCellValue();
+            System.out.println("birthdate :" + birthdate);
+        }
+
+        myExcelBook.close();
+
+        return false;
+    }
+
+}
