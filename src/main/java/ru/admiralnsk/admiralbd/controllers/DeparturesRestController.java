da9012facac916ec16e361c3f9ecd251994ba4ad
poipoilion <poipoilion@users.noreply.github.com>
2021-04-13 19:35:46 +0600

    Add treeTable without data

--- src/main/java/ru/admiralnsk/admiralbd/controllers/DeparturesRestController.java
+++ src/main/java/ru/admiralnsk/admiralbd/controllers/DeparturesRestController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.admiralnsk.admiralbd.models.Node;
 import ru.admiralnsk.admiralbd.services.DepartureService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,4 +22,21 @@ public class DeparturesRestController {
     public List<String> getDistinctConsignors(@RequestParam("departureWay") String departureWay) {
         return departureService.findDistinctConsignorsByDepartureWay(departureWay);
     }
+    @GetMapping("/nodes")
+    public List<Node> nodes() {
+        return getSampleNodeList();
+    }
+
+    private List<Node> getSampleNodeList() {
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(new Node("Root", "0", "Root", "http://frontbackend.com/"));
+        nodes.add(new Node("Child1", "Root", "Child1", "http://frontbackend.com/child1"));
+        nodes.add(new Node("Child2", "Root", "Child2", "http://frontbackend.com/child2"));
+        nodes.add(new Node("Child3", "Root", "Child3", "http://frontbackend.com/child3"));
+        nodes.add(new Node("Child3.1", "Child3", "Child3.1", "http://frontbackend.com/child3/child1"));
+        nodes.add(new Node("Child3.2", "Child3", "Child3.2", "http://frontbackend.com/child3/child2"));
+        nodes.add(new Node("Child4", "Root", "Child4", "http://frontbackend.com/child4"));
+        return nodes;
+    }
 }
+
