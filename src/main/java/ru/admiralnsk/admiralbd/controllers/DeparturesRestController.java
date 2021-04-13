0ae626314d6ce69cebe2fbf319e5f481173770cc
darkace <dan270799@gmail.com>
2021-04-13 17:40:53 +0300

    tree tables both

--- src/main/java/ru/admiralnsk/admiralbd/controllers/DeparturesRestController.java
+++ src/main/java/ru/admiralnsk/admiralbd/controllers/DeparturesRestController.java
@@ -22,14 +22,32 @@ public class DeparturesRestController {
     public List<String> getDistinctConsignors(@RequestParam("departureWay") String departureWay) {
         return departureService.findDistinctConsignorsByDepartureWay(departureWay);
     }
-    @GetMapping("/nodes")
-    public List<Node> nodes() {
-        return getSampleNodeList();
+
+    @GetMapping("/departureStationRfTreeTable")
+    public List<Node> departureStationRf() {
+        return getDepartureStationRfNodes();
     }
 
-    private List<Node> getSampleNodeList() {
+    @GetMapping("/ownersTreeTable")
+    public List<Node> owners() {
+        return getOwnersNodes();
+    }
+
+    private List<Node> getDepartureStationRfNodes() {
         List<Node> nodes = new ArrayList<>();
         nodes.add(new Node("Root", "0", "Root", "http://frontbackend.com/"));
+        nodes.add(new Node("Child1", "Root", "Child1", "http://frontbackend.com/child1"));
+        nodes.add(new Node("Child2", "Root", "Child2", "http://frontbackend.com/child2"));
+        nodes.add(new Node("Child3", "Root", "Child3", "http://frontbackend.com/child3"));
+        nodes.add(new Node("Child3.1", "Child3", "Child3.1", "http://frontbackend.com/child3/child1"));
+        nodes.add(new Node("Child3.2", "Child3", "Child3.2", "http://frontbackend.com/child3/child2"));
+        nodes.add(new Node("Child4", "Root", "Child4", "http://frontbackend.com/child4"));
+        return nodes;
+    }
+
+    private List<Node> getOwnersNodes() {
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(new Node("Root", "0", "Roqweqweot", "http://frontbackend.com/"));
         nodes.add(new Node("Child1", "Root", "Child1", "http://frontbackend.com/child1"));
         nodes.add(new Node("Child2", "Root", "Child2", "http://frontbackend.com/child2"));
         nodes.add(new Node("Child3", "Root", "Child3", "http://frontbackend.com/child3"));
