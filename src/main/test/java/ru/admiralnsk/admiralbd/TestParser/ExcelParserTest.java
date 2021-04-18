5d892065f1ffe70452f6fb7a522be16fd1e6efe5
darkace <dan270799@gmail.com>
2021-04-18 17:31:10 +0300

    small fixes

--- src/main/test/java/ru/admiralnsk/admiralbd/TestParser/ExcelParserTest.java
+++ src/main/test/java/ru/admiralnsk/admiralbd/TestParser/ExcelParserTest.java
@@ -1,23 +1,19 @@
 package ru.admiralnsk.admiralbd.TestParser;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import org.testng.Assert;
 import ru.admiralnsk.admiralbd.mappers.DepartureFieldsMapper;
 import ru.admiralnsk.admiralbd.models.Departure;
-
-import java.io.*;
-import java.text.DateFormat;
-import java.util.*;
-
-import org.springframework.mock.web.MockMultipartFile;
 import ru.admiralnsk.admiralbd.parser.ExcelParser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 
 public class ExcelParserTest {
@@ -26,123 +22,12 @@ public class ExcelParserTest {
     public void readFromExcel() throws IOException {
 
         ExcelParser excelParser = new ExcelParser(new DepartureFieldsMapper());
-        MultipartFile file = new MockMultipartFile("TestExcel.xlsx", new FileInputStream(new File("src/main/test/resources/TestExcel.xlsx")));
-        List<Departure> departureList = new ArrayList<>();
+        MultipartFile file = new MockMultipartFile("TestExcel.xlsx", new FileInputStream("src/main/test/resources/TestExcel.xlsx"));
+
+        List<Departure> departureList = this.createDepartures(new ArrayList<>());
+
         List<Departure> departureListTest = excelParser.readFromExcel(file);
-        Departure departureOne = Departure.builder()
-                .departureDate((new GregorianCalendar(2020, 0, 1)).getTime())
-                .carriageNumber(29064888)
-                .documentNumber("28392746")
-                .arrivalDate((new GregorianCalendar(2020, 0, 6)).getTime())
-                .lendingDate((new GregorianCalendar(2020, 0, 6)).getTime())
-                .transportationType("экспорт")
-                .cargo(13302)
-                .cargoType("ИЗДЕЛИЯ БУМ ПР")
-                .departureCountry("РОССИЯ")
-                .departureStationCIS("КОЙТЫ")
-                .departureStationCISCode(28410)
-                .departureRegion("Республика Коми")
-                .departureWay("СЕВ")
-                .departureStationRF("КОЙТЫ")
-                .departureStationRFCode(28410)
-                .consignor("ОАО СЫКТЫВКАР ТИССЬЮ ГРУП")
-                .consignorACEO(15093929)
-                .destinationCountry("КАЗАХСТАН")
-                .destinationRegion("Челябинская область")
-                .destinationWay("ЮУР")
-                .destinationStationRF("КАРТАЛЫ 1-ЭК")
-                .destinationStationRFCode(81650)
-                .destinationStationCIS("КАРАГАНДЫ")
-                .destinationStationCISCode(67390)
-                .consignee(null)
-                .consigneeACEO(0)
-                .carriageKind("Крытые")
-                .carriageType("0202 4-ОС КРЫТЫЙ УНИВЕРСАЛ")
-                .payer("ОАО  Сыктывкар Тиссью Груп")
-                .owner("РГ Лизинг ООО")
-                .renter("ООО ЖДК ТРАНЗИТ")
-                .operator("ЖДК Транзит")
-                .carriageKm(2090)
-                .volume(9)
-                .rate(87119)
-                .build();
-        departureList.add(departureOne);
-        Departure departureTwo = Departure.builder()
-                .departureDate((new GregorianCalendar(2020, 0, 1)).getTime())
-                .carriageNumber(29067337)
-                .documentNumber("ЭЦ131476")
-                .arrivalDate((new GregorianCalendar(2020, 0, 8)).getTime())
-                .lendingDate((new GregorianCalendar(2020, 0, 9)).getTime())
-                .transportationType("внутренние")
-                .cargo(54102)
-                .cargoType("КОМБИКОРМА ВС")
-                .departureCountry("РОССИЯ")
-                .departureStationCIS(null)
-                .departureStationCISCode(10)
-                .departureRegion("Забайкальский край")
-                .departureWay("ЗАБ")
-                .departureStationRF("ЗАБАЙКАЛЬСК")
-                .departureStationRFCode(94680)
-                .consignor("ООО РУС ЛОГИСТИК")
-                .consignorACEO(24735203)
-                .destinationCountry("РОССИЯ")
-                .destinationRegion("Новосибирская область")
-                .destinationWay("ЗСБ")
-                .destinationStationRF("НОВОСИБ-ВОСТ")
-                .destinationStationRFCode(85150)
-                .destinationStationCIS(null)
-                .destinationStationCISCode(null)
-                .consignee("ООО НОВОСИБСКЛАДСЕРВИС")
-                .consigneeACEO(71484289)
-                .carriageKind("Крытые")
-                .carriageType("0202 4-ОС КРЫТЫЙ УНИВЕРСАЛ")
-                .payer("Общество с ограниченной ответственностью Рус Логистик")
-                .owner("Сибирский Край ООО")
-                .renter("вне аренды")
-                .operator("ООО Сибирский край")
-                .carriageKm(3318)
-                .volume(66)
-                .rate(109428)
-                .build();
-        departureList.add(departureTwo);
-        Departure departureThree = Departure.builder()
-                .departureDate((new GregorianCalendar(2020, 0, 1)).getTime())
-                .carriageNumber(28037141)
-                .documentNumber("Г0104948")
-                .arrivalDate((new GregorianCalendar(2020, 0, 7)).getTime())
-                .lendingDate((new GregorianCalendar(2020, 0, 7)).getTime())
-                .transportationType("транзит")
-                .cargo(42103)
-                .cargoType("КАРТОН Д/ГОФРЫ")
-                .departureCountry("КАЗАХСТАН")
-                .departureStationCIS("СОРОКОВАЯ")
-                .departureStationCISCode(69020)
-                .departureRegion("Челябинская область")
-                .departureWay("ЮУР")
-                .departureStationRF("КАРТАЛЫ 1-ЭК")
-                .departureStationRFCode(81650)
-                .consignor(null)
-                .consignorACEO(0)
-                .destinationCountry("БЕЛАРУСЬ")
-                .destinationRegion("Смоленская область")
-                .destinationWay("МСК")
-                .destinationStationRF("КРАСНОЕ-ЭКСП")
-                .destinationStationRFCode(17140)
-                .destinationStationCIS("КОЛЯДИЧИ")
-                .destinationStationCISCode(14480)
-                .consignee(null)
-                .consigneeACEO(0)
-                .carriageKind("Крытые")
-                .carriageType("0208 4-ОС КР ОБЪЕМ КУЗ 140 КУБ.М С УШИР ДВЕРН ПРОЕМ")
-                .payer("Акционерное общество НефтеТрансСервис")
-                .owner("инвентарный парк")
-                .renter(null)
-                .operator("неизвестен")
-                .carriageKm(2550)
-                .volume(0)
-                .rate(50062)
-                .build();
-        departureList.add(departureThree);
+
         Assert.assertEquals(departureListTest.size(), departureList.size());
         for (int i = 0; i < departureList.size(); i++) {
             Assert.assertEquals(departureListTest.get(i).getDepartureDate(), departureList.get(i).getDepartureDate());
@@ -182,5 +67,126 @@ public class ExcelParserTest {
             Assert.assertEquals(departureListTest.get(i).getVolume(), departureList.get(i).getVolume());
             Assert.assertEquals(departureListTest.get(i).getRate(), departureList.get(i).getRate());
         }
+    }
+
+    private List<Departure> createDepartures(List<Departure> departureList) {
+        Departure departureOne = Departure.builder()
+                .departureDate((new GregorianCalendar(2020, Calendar.JANUARY, 1)).getTime())
+                .carriageNumber(29064888)
+                .documentNumber("28392746")
+                .arrivalDate((new GregorianCalendar(2020, Calendar.JANUARY, 6)).getTime())
+                .lendingDate((new GregorianCalendar(2020, Calendar.JANUARY, 6)).getTime())
+                .transportationType("экспорт")
+                .cargo(13302)
+                .cargoType("ИЗДЕЛИЯ БУМ ПР")
+                .departureCountry("РОССИЯ")
+                .departureStationCIS("КОЙТЫ")
+                .departureStationCISCode(28410)
+                .departureRegion("Республика Коми")
+                .departureWay("СЕВ")
+                .departureStationRF("КОЙТЫ")
+                .departureStationRFCode(28410)
+                .consignor("ОАО СЫКТЫВКАР ТИССЬЮ ГРУП")
+                .consignorACEO(15093929)
+                .destinationCountry("КАЗАХСТАН")
+                .destinationRegion("Челябинская область")
+                .destinationWay("ЮУР")
+                .destinationStationRF("КАРТАЛЫ 1-ЭК")
+                .destinationStationRFCode(81650)
+                .destinationStationCIS("КАРАГАНДЫ")
+                .destinationStationCISCode(67390)
+                .consignee(null)
+                .consigneeACEO(0)
+                .carriageKind("Крытые")
+                .carriageType("0202 4-ОС КРЫТЫЙ УНИВЕРСАЛ")
+                .payer("ОАО  Сыктывкар Тиссью Груп")
+                .owner("РГ Лизинг ООО")
+                .renter("ООО ЖДК ТРАНЗИТ")
+                .operator("ЖДК Транзит")
+                .carriageKm(2090)
+                .volume(9)
+                .rate(87119)
+                .build();
+        departureList.add(departureOne);
+
+        Departure departureTwo = Departure.builder()
+                .departureDate((new GregorianCalendar(2020, Calendar.JANUARY, 1)).getTime())
+                .carriageNumber(29067337)
+                .documentNumber("ЭЦ131476")
+                .arrivalDate((new GregorianCalendar(2020, Calendar.JANUARY, 8)).getTime())
+                .lendingDate((new GregorianCalendar(2020, Calendar.JANUARY, 9)).getTime())
+                .transportationType("внутренние")
+                .cargo(54102)
+                .cargoType("КОМБИКОРМА ВС")
+                .departureCountry("РОССИЯ")
+                .departureStationCIS(null)
+                .departureStationCISCode(10)
+                .departureRegion("Забайкальский край")
+                .departureWay("ЗАБ")
+                .departureStationRF("ЗАБАЙКАЛЬСК")
+                .departureStationRFCode(94680)
+                .consignor("ООО РУС ЛОГИСТИК")
+                .consignorACEO(24735203)
+                .destinationCountry("РОССИЯ")
+                .destinationRegion("Новосибирская область")
+                .destinationWay("ЗСБ")
+                .destinationStationRF("НОВОСИБ-ВОСТ")
+                .destinationStationRFCode(85150)
+                .destinationStationCIS(null)
+                .destinationStationCISCode(null)
+                .consignee("ООО НОВОСИБСКЛАДСЕРВИС")
+                .consigneeACEO(71484289)
+                .carriageKind("Крытые")
+                .carriageType("0202 4-ОС КРЫТЫЙ УНИВЕРСАЛ")
+                .payer("Общество с ограниченной ответственностью Рус Логистик")
+                .owner("Сибирский Край ООО")
+                .renter("вне аренды")
+                .operator("ООО Сибирский край")
+                .carriageKm(3318)
+                .volume(66)
+                .rate(109428)
+                .build();
+        departureList.add(departureTwo);
+
+        Departure departureThree = Departure.builder()
+                .departureDate((new GregorianCalendar(2020, Calendar.JANUARY, 1)).getTime())
+                .carriageNumber(28037141)
+                .documentNumber("Г0104948")
+                .arrivalDate((new GregorianCalendar(2020, Calendar.JANUARY, 7)).getTime())
+                .lendingDate((new GregorianCalendar(2020, Calendar.JANUARY, 7)).getTime())
+                .transportationType("транзит")
+                .cargo(42103)
+                .cargoType("КАРТОН Д/ГОФРЫ")
+                .departureCountry("КАЗАХСТАН")
+                .departureStationCIS("СОРОКОВАЯ")
+                .departureStationCISCode(69020)
+                .departureRegion("Челябинская область")
+                .departureWay("ЮУР")
+                .departureStationRF("КАРТАЛЫ 1-ЭК")
+                .departureStationRFCode(81650)
+                .consignor(null)
+                .consignorACEO(0)
+                .destinationCountry("БЕЛАРУСЬ")
+                .destinationRegion("Смоленская область")
+                .destinationWay("МСК")
+                .destinationStationRF("КРАСНОЕ-ЭКСП")
+                .destinationStationRFCode(17140)
+                .destinationStationCIS("КОЛЯДИЧИ")
+                .destinationStationCISCode(14480)
+                .consignee(null)
+                .consigneeACEO(0)
+                .carriageKind("Крытые")
+                .carriageType("0208 4-ОС КР ОБЪЕМ КУЗ 140 КУБ.М С УШИР ДВЕРН ПРОЕМ")
+                .payer("Акционерное общество НефтеТрансСервис")
+                .owner("инвентарный парк")
+                .renter(null)
+                .operator("неизвестен")
+                .carriageKm(2550)
+                .volume(0)
+                .rate(50062)
+                .build();
+        departureList.add(departureThree);
+
+        return departureList;
     }
 }
